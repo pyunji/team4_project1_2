61cba6677f6bc3546c0d3ab8c8269d3e5c6add4f
Pyunji <vusguswl@gmail.com>
2021-10-19 15:04:13 +0900

    initial commit

---
+++ src/test/java/com/mycompany/webapp/Team4Project12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4Project12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
